8bd649184dfae79599aa157986810a5751261c87
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-14 10:50:39 -0600

    add Admin model and ContactMessage model, complete controllers, migrate, seed

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,34 @@
+class AdminsController < ApplicationController
+    def index
+        @admins = Admin.all
+        render json: @admins
+    end
+
+    def show
+        @admin = Admin.find(params[:id])
+        render json: @admin
+    end
+
+    def update
+        found_admin = Admin.find(params[:id])
+        @updated_admin = found_admin.update(admin_params)
+        render json: @updated_admin
+    end
+
+    def create
+        @new_admin = Admin.create(admin_params)
+        render json: @new_admin
+    end
+
+    def destroy
+        found_admin = Admin.find(params[:id])
+        @deleted_admin = found_admin.destroy
+        render json: @deleted_admin
+    end
+
+    private
+
+    def admin_params
+        params.require(:admin).permit(:username, :password)
+    end
+end
---
+++ app/controllers/contact_messages_controller.rb
@@ -0,0 +1,28 @@
+class ContactMessagesController < ApplicationController
+    def index
+        @contact_messages = ContactMessage.all
+        render json: @contact_messages
+    end
+
+    def show
+        @contact_message = ContactMessage.find(params[:id])
+        render json: @contact_message
+    end
+
+    def create
+        @new_contact_message = ContactMessage.create(
+            full_name: params[:full_name],
+            email: params[:email],
+            phone_number: params[:phone_number],
+            address: params[:address],
+            message: params[:message]
+        )
+        render json: @new_contact_message
+    end
+
+    def destroy
+        found_contact_message = ContactMessage.find(params[:id])
+        @deleted_contact_message = found_contact_message.destroy
+        render json: @deleted_contact_message
+    end
+end
