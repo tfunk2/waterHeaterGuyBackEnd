2b00b182b0f526f579b9c9ed452a889b1037ef70
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-26 19:14:06 -0600

    Add opened_status column to contact_messages and testimonials

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -10,9 +10,18 @@ class TestimonialsController < ApplicationController
     def create
         @new_testimonial = Testimonial.create(
             name: params[:name],
-            message: params[:message]
+            message: params[:message],
+            opened_status: "unopened"
         )
         render json: @new_testimonial
+    end
+
+    def update
+        find_testimonial = Testimonial.find(params[:id])
+        @updated_testimonial = find_testimonial.update(
+            opened_status: params[:opened_status]
+        )
+        render json: @updated_testimonial
     end
 
     def destroy
